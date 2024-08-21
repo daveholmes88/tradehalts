d522b9efe6180fefbdde9cbfe638ced74c8c9020
Dave Holmes <daveholmes88@gmail.com>
2024-08-21 11:07:06 -0500

    created table

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -16,11 +16,35 @@ export default function Home() {
                 console.error('Error fetching trade halt data:', error);
             });
     }, []);
-    
+
   return (
     <div>
-        <h1>Hello World</h1>
-        {data.map(d => <li>{d.symbol}</li>)}
+        <TableContainer component={Paper}>
+          <Table stickyHeader sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Symbol</TableCell>
+            <TableCell>Reason Code</TableCell>
+            <TableCell>Halt Time</TableCell>
+            <TableCell>Resume Time</TableCell>
+            <TableCell>Publication Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((d) => (
+            <TableRow key={data.symbol}>
+              <TableCell component="th" scope="row">
+                {d.symbol}
+              </TableCell>
+              <TableCell>{d.reason_code}</TableCell>
+              <TableCell>{d.halt_time}</TableCell>
+              <TableCell>{d.resume_time}</TableCell>
+              <TableCell>{d.pub_date}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 }
